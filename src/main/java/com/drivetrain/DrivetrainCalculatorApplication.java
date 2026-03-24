a9acc5bd2706a7657c5c9af8522350edde7fb3cf
lamphnam <nampham2131@gmail.com>
2026-03-24 14:56:37 +0700

    first commit

---
+++ src/main/java/com/drivetrain/DrivetrainCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.drivetrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivetrainCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivetrainCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drivetrain/DrivetrainCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivetrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivetrainCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
